8f43a30da5ecdcea40f7c88bc8d3107960d21aef
Jonathan Carbajal <jon.carbajal.ei@gmail.com>
2014-04-08 15:05:48 -0400

    Checking cumulative sums, breaking analysis into constituent parts

--- IDN-analysis/PostProcessing/GeospatialOverlayPrep.R
+++ IDN-analysis/PostProcessing/GeospatialOverlayPrep.R
@@ -5,7 +5,8 @@ require(WriteXLS)
 require(ggplot2)
 require(plyr)
 
-metrics_local <- read.csv('~/Dropbox/Indonesia Geospatial Analysis/Data Modeling and Analysis/NPoutputs/April 2014/ALL-metrics-local-lite.csv')
+#metrics_local <- read.csv('~/Dropbox/Indonesia Geospatial Analysis/Data Modeling and Analysis/NPoutputs/April 2014/ALL-metrics-local-lite.csv')
+metrics_local <- read.csv('~/Dropbox/Indonesia Geospatial Analysis/Data Modeling and Analysis/NPoutputs/April 2014/ALL-metrics-local-lite-V2.csv')
 rollout <- read.csv('~/Dropbox/Indonesia Geospatial Analysis/Data Modeling and Analysis/NPoutputs/April 2014/ALL-networks-proposed-with-rollout.csv')
 
 #combining sequenced grid settlements and non-grid points all together
@@ -17,28 +18,38 @@ all_settlements_1kmbuffer <- subset(all_settlements, Metric...System == 'grid')
 #Order by Cabang then sequence
 all_settlements <- all_settlements_1kmbuffer[with(all_settlements_1kmbuffer, order(Pln_cabang, seq_fs)),]
 
+#Assign Scenario tag each settlement
+all_settlements$scenario <- as.character(all_settlements$Pln_cabang)
+replacements <- as.character(all_settlements$Ei_subarea[which(all_settlements$Pln_cabang == "Cabang Tual")])
+all_settlements$scenario[which(all_settlements$Pln_cabang == "Cabang Tual")] <- replacements
+
+
 #Recalculate Cumulative Averages
 all_settlements$CumulativeHH <- NA
 all_settlements$CumulativeMV <- NA
 Cabangs <- unique(all_settlements$Pln_cabang)
+EI_areas <- unique(all_settlements$Ei_subarea)
+scenarios <- unique(all_settlements$scenario)
 
-all_settlements$dist.N.19.11[which(is.na(all_settlements$dist.N.19.11))] <- 0 
+all_settlements$dist.N.19.11[which(is.na(all_settlements$dist))] <- 0 
 
-
-for (j in 1:length(Cabangs)){
-#define cumulative household
-  all_settlements$CumulativeHH[which(all_settlements$Pln_cabang == Cabangs[j])] <-
-    cumsum(all_settlements$Demographics...Projected.household.count[which(all_settlements$Pln_cabang == Cabangs[j])])
+for (j in 1:length(scenarios)){
+  #define cumulative household
+  all_settlements$CumulativeHH[which(all_settlements$scenario == scenarios[j])] <-
+    cumsum(all_settlements$Demand..household....Target.household.count[which(all_settlements$scenario == scenarios[j])])
   #define cumulative MV installed
-  all_settlements$CumulativeMV[which(all_settlements$Pln_cabang == Cabangs[j])] <- 
-    cumsum(all_settlements$dist.N.19.11[which(all_settlements$Pln_cabang == Cabangs[j])])
+  all_settlements$CumulativeMV[which(all_settlements$scenario == scenarios[j])] <- 
+    cumsum(all_settlements$dist.N.19.11[which(all_settlements$scenario == scenarios[j])])
   
 }
-all_settlements <- mutate(all_settlements, averageMVperHH =CumulativeMV/CumulativeHH)
+all_settlements <- mutate(all_settlements, averageMVperHH =CumulativeMV/CumulativeHH,
+                          MVperHH = dist.N.19.11/Demand..household....Target.household.count,
+                          averageMVperHH_old = Cumulative/Cumulati_1)
+
 
 #Write for XLS
 WriteXLS("all_settlements_1kmbuffer","~/Dropbox/Indonesia Geospatial Analysis/Data Modeling and Analysis/NPoutputs/April 2014/All-Settlements-for-Electrification.xls")
-# # 
+## 
 
 h = c(12,15,18) # Y intercept Cutoff
 
@@ -46,11 +57,11 @@ plot_pln_cabangs <-
   ggplot(data= all_settlements, 
          aes(x=PercentOfN.N.19.15, 
              y=averageMVperHH, 
-             colour = Pln_cabang)) +
+             colour = scenario)) +
   geom_line() +
-  labs(title = "MV Line Per Household", 
+  labs(title = "Cumulative Moving Average of MV Line Per Household", 
        x = "Percent of New Households Connected [%]", 
-       y="Cumulative Moving Average of MV Line [m/HH]", 
+       y="MV Line [m/HH]", 
        colour = "PLN Cabang") +
   theme(text=element_text(size=40),
         legend.text = element_text(size=30),
@@ -60,5 +71,35 @@ plot_pln_cabangs <-
         panel.background=element_blank(),
         legend.position=c(0,1), #x=0=left, y=1=top
         legend.justification=c(0,1)) + 
-  geom_hline(yintercept=c(h,15,18), colour="#636363", linetype="dashed") + 
-  geom_text(aes(0.25, h, label = h, vjust = -1), size = 12)
+  geom_hline(yintercept=c(h,15,18), colour="#636363", linetype="dashed") 
+plot_pln_cabangs
+
+#Determine the geospatial overlay categorization
+
+for (j in 1:length(scenarios)){
+  #Seqence spots more than 
+  
+  all_settlements[which(all_settlements$scenario == scenarios[j]),] <- 
+    mutate(all_settlements[which(all_settlements$scenario == scenarios[j]),],
+           
+           
+           
+           )
+  
+  
+  for (j in 1:length(scenarios)){
+    #define cumulative household
+    all_settlements$CumulativeHH[
+      which(all_settlements$scenario == scenarios[j])[
+        which]
+      ]
+  }
+  
+  
+  all_settlements$seq_fs[which(all_settlements$averageMVperHH_old >12 == scenarios[j])] <-
+    cumsum(all_settlements$Demand..household....Target.household.count[which(all_settlements$scenario == scenarios[j])])
+  #define cumulative MV installed
+  all_settlements$CumulativeMV[which(all_settlements$scenario == scenarios[j])] <- 
+    cumsum(all_settlements$dist.N.19.11[which(all_settlements$scenario == scenarios[j])])
+  
+}
